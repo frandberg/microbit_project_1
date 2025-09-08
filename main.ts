d380051cb5187940c501d197da4a1ca8225b9503
KarlHampus <129685919+KarlHampus@users.noreply.github.com>
2025-09-08 10:12:30 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+function test () {
+    while (x < 5) {
+        while (y < 5) {
+            led.plot(x, y)
+            y = y + 1
+            basic.pause(50)
+        }
+        x = x + 1
+    }
+}
 let last_time = 0
+let y = 0
+let x = 0
 let velocity = 0
 let position = 0
 let delat_time = 0
+test()
 basic.forever(function () {
     delat_time = input.runningTime() - last_time
     velocity = input.acceleration(Dimension.X) * 0.00982 * (delat_time / 1000)
