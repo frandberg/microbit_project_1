541818a2c5476c0da88cbbdcf1c359c838f74aeb
frandberg <42117132+frandberg@users.noreply.github.com>
2025-09-09 14:32:20 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,31 @@
-function create_message (reciever: number, kind: number, contents: string) {
-    _message = "" + reciever + "|" + kind + "|" + contents
-    return _message
+function send_message (reciever: number, kind: number, contents: string) {
+    radio.sendString("" + reciever + "|" + kind + "|" + contents)
+}
+// "dealer" or "player"
+function init_list_values () {
+    suits = [
+    "H",
+    "D",
+    "C",
+    "S"
+    ]
+    card_values_alpha = [
+    "J",
+    "Q",
+    "K",
+    "A"
+    ]
+    card_values = [
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+    ]
 }
 function get_message_contents (message: string) {
     for (let _k = 0; _k <= message.length - 1; _k++) {
@@ -28,6 +53,11 @@ function get_message_kind (message: string) {
     }
     return -1
 }
+input.onButtonPressed(Button.A, function () {
+    if (game_stage == GAME_STAGE_ROLE_SELECTION) {
+        select_role(ROLE_DEALER)
+    }
+})
 radio.onReceivedString(function (msg) {
     // Dealer receives join requests while finding players
     _reciever = get_message_reciever(msg)
@@ -47,45 +77,6 @@ function get_message_reciever (message: string) {
     }
     return -1
 }
-function msg_recieved_dealer (sender: number, msg_kind: number, msg_contents: string) {
-	
-}
-function msg_recieved_player (sender: number, msg_kind: number, msg_contents: string) {
-	
-}
-/**
- * "dealer" or "player"
- */
-function init_list_values () {
-    suits = [
-    "H",
-    "D",
-    "C",
-    "S"
-    ]
-    card_values_alpha = [
-    "J",
-    "Q",
-    "K",
-    "A"
-    ]
-    card_values = [
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10
-    ]
-}
-input.onButtonPressed(Button.A, function () {
-    if (game_stage == GAME_STAGE_ROLE_SELECTION) {
-        select_role(ROLE_DEALER)
-    }
-})
 function build_card_list () {
     cards = []
     i = 0
@@ -106,11 +97,36 @@ function init_constants () {
     ROLE_DEALER = 0
     ROLE_PLAYER = 1
 }
+
+function add_player(player_id: number){
+    players.push(player_id)
+}
+function get_player_index(player_id: number): number{
+    for (let _l = 0; _l < players.length; _l++){
+        if(players[_l] == player_id){
+            return _l
+        }
+    }
+    return -1
+}
+
+
+function msg_recieved_dealer (sender: number, msg_kind: number, msg_contents: string) {
+	if(msg_kind == MSG_PLAYER_JOIN){
+        add_player(radio.receivedPacket(RadioPacketProperty.SerialNumber))
+    }
+}
 input.onButtonPressed(Button.B, function () {
     if (game_stage == GAME_STAGE_ROLE_SELECTION) {
         select_role(ROLE_PLAYER)
+        send_message(0, MSG_PLAYER_JOIN, "")
+
+        
     }
 })
+function msg_recieved_player (sender: number, msg_kind: number, msg_contents: string) {
+    
+}
 function select_role (role: number) {
     game_stage = GAME_STAGE_FINDING_PLAYERS
     role = role
@@ -125,32 +141,36 @@ function select_role (role: number) {
     basic.showString(_display_char)
 }
 let _display_char = ""
-let role = 0
-let ROLE_PLAYER = 0
 let GAME_STAGE_FINDING_PLAYERS = 0
-let i = 0
-let ROLE_DEALER = 0
-let card_values: number[] = []
-let card_values_alpha: string[] = []
-let suits: string[] = []
-let cards: string[] = []
 let GAME_STAGE_ROLE_SELECTION = 0
-let game_stage = 0
 
+let i = 0
 let _reciever = 0
 let _last_delimeter_index2 = 0
 let _delimeters_found2 = 0
 let _last_delimeter_index = 0
 let _delimeters_found = 0
+let card_values: number[] = []
+let card_values_alpha: string[] = []
+let suits: string[] = []
 let _message = ""
+let cards: string[] = []
+let dealer_id = 0
+
+let game_stage = 0
+let role = 0
+let ROLE_DEALER = 0
+let ROLE_PLAYER = 1
+let MSG_PLAYER_JOIN = 0
 let serial_number = 0
 serial_number = control.deviceSerialNumber()
-ROLE_PLAYER = 1
 role = ROLE_DEALER
+let players : number[] = []
 init_constants()
 game_stage = GAME_STAGE_ROLE_SELECTION
 init_list_values()
 build_card_list()
+
 for (let värde of cards) {
     datalogger.log(datalogger.createCV(värde, värde))
 }
@@ -164,4 +184,3 @@ basic.forever(function () {
     	
     }
 })
-
